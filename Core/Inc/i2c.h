adb227e4f7746c8930dd55f013b4c881d97526fc
randomlygenerated1234 <s234490@dtu.dk>
2025-06-10 12:19:15 +0200

    compacted i2c stuff to files

---
+++ Core/Inc/i2c.h
@@ -0,0 +1,13 @@
+/*
+ * i2c.h
+ *
+ *  Created on: 10. jun. 2025
+ *      Author: theis
+ */
+
+#ifndef INC_I2C_H_
+#define INC_I2C_H_
+
+void I2C_Scan(I2C_HandleTypeDef *hi2c);
+
+#endif /* INC_I2C_H_ */
