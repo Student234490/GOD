e2e341fb8c75c68f39b76e2527e082d6c159e8bd
rasmus142i <rasmus.jensen11@gmail.com>
2025-06-11 09:05:59 +0200

    magnet i 16.16, lut med cos, mere vector math
    yeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeet

--- Core/Inc/fixp.h
+++ Core/Inc/fixp.h
@@ -16,8 +16,22 @@
 #define FIX14_DIV(a, b)  ( ((a) << FIX14_SHIFT) / b )
 
 #define FIX16_SHIFT 16
-#define FIX16_MULT(a, b) ( (int32_t)(((int64_t)(a) * (int64_t)(b)) >> FIX16_SHIFT) ) // No change; keep intermediate 64-bit multiplication.
-#define FIX16_DIV(a, b)  ( (int32_t)(((int64_t)(a) << FIX16_SHIFT) / (int64_t)(b)) ) // Still uses 64-bit intermediate.
+#define FIX16_MULT(a, b) ((int32_t)(((int64_t)(a) * (int64_t)(b)) >> FIX16_SHIFT))
+#define FIX16_POW(a, exp_int) ({ \
+    int32_t _base = (a); \
+    int32_t _result = 1 << FIX16_SHIFT; \
+    int _n = (exp_int) >> FIX16_SHIFT; /* Extract integer value */ \
+    if (_n < 0) { \
+        _n = -_n; \
+        _base = FIX16_DIV(1 << FIX16_SHIFT, _base); \
+    } \
+    while (_n) { \
+        if (_n & 1) _result = FIX16_MULT(_result, _base); \
+        _base = FIX16_MULT(_base, _base); \
+        _n >>= 1; \
+    } \
+    _result; \
+})
 
 
 /* Exported functions -------------------------------------------------- */
